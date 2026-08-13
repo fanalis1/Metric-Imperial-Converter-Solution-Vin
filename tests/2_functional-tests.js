ae2d510e8f0bc502f0079ae34f2376feb0d4edb7
Vincent <32320764+fanalis1@users.noreply.github.com>
2026-08-13 09:30:22 +0700

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,7 +2,7 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 let assert = chai.assert;
 const server = require("../server");
-
+*
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
