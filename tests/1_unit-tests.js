77b87c51f6e91a5e2214e068c8101452f5372523
Vincent <32320764+fanalis1@users.noreply.github.com>
2026-08-13 09:30:08 +0700

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,7 +3,7 @@ let assert = chai.assert;
 const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
-
+*
 suite("Unit Tests", function () {
   test("Testing valid whole number input", () => {
     assert.strictEqual(
