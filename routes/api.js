0872a6e73e3fb16ea64d6ee9f6a18963f7511fb9
Vincent <32320764+fanalis1@users.noreply.github.com>
2026-08-13 09:29:34 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
-
+*
   app.route("/api/convert").get((req, res) => {
     const { input } = req.query;
     if (!input) {
